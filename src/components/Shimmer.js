5b487d1a8ef548b10b3d3df2b57395c472dfced3
rahulsbeelur <srahulbeelur@gmail.com>
2024-01-04 14:38:00 +0530

    Shimmer bug fix

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,6 +1,6 @@
 const Shimmer = () => {
     return (
-        <div className="shimmer-container max-w-[1200px] mt-10 mx-auto">
+        <div className="shimmer-container max-w-[1200px] mx-auto">
             <div className="shimmer-card"></div>
             <div className="shimmer-card"></div>
             <div className="shimmer-card"></div>
