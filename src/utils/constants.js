0d934d08a55e00af137ae51ee457fcd062b8a0a9
rahulsbeelur <srahulbeelur@gmail.com>
2024-01-07 11:59:45 +0530

    CORS proxy url change

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,7 @@
 export const CDN_URL =
     'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/';
 
-export const CORS_PROXY = 'https://api.codetabs.com/v1/proxy?quest=';
+export const CORS_PROXY = 'https://api.allorigins.win/raw?url=';
+
+// export const CORS_PROXY = 'https://api.codetabs.com/v1/proxy?quest=';
+// export const CORS_PROXY = 'https://thingproxy.freeboard.io/fetch/';
